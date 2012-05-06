3fe8d7af639a5afba4b54f5074b9cfc22c0f74be
Den Raskovalov <denplusplus@yandex-team.ru>
2012-05-06 22:22:22 +0400

    almost done 383

--- euler383/euler383.py
+++ euler383/euler383.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+import sys
+
+if False:
+    for p in (5, 25):
+        for i in xrange(0, p + 10):
+            print i, p, (2*i - 1)/p - 2*(i/p)
+
+    sys.exit(0)
+
 def sum(i):
     power = 5
     res = 0
@@ -8,9 +17,55 @@ def sum(i):
         power *= 5
     return res
 
-N = 1000
+def mod5(i):
+    if 0 != i:
+        return mod5(i/5) + chr(i % 5 + ord('0'))
+    else:
+        return ""
+
+def mod5_2(i, k):
+    if 0 != i:
+        if i % 5 != k:
+            return mod5_2(i/5, k) + chr(i % 5 + ord('0'))
+        else:
+            return mod5_2(i/5, k)
+    else:
+        return ""
+
+def num5(i, k):
+    if 0 != i:
+        return num5(i/5, k) if i % 5 != k else 1 + num5(i/5, k)
+    else:
+        return 0
+
+N = 1000000
+if False:
+    count = 0
+    for i in xrange(1, N + 1):
+        if num5(2*i - 1, 4) >= num5(i, 0):
+            count += 1
+    print count
+    count = 0
+
 count = 0
 for i in xrange(1, N + 1):
+    # if sum(2*i - 1) < 2*sum(i):
+    #     print "!",
+    # print mod5(i/5), mod5((2*i - 1)/5), i, 2*i - 1
+    if False:
+        # print sum(2*i - 1), 2*sum(i)
+        for j in xrange(1, 5):
+            print "", 2*(i/(5 ** j)), (2*i - 1)/(5 ** j)
     if sum(2*i - 1) < 2*sum(i):
         count += 1
-print count
+        # print i, mod5(i), num5(i, 0)
+    d = sum(2*i) - 2*sum(i)
+    nz = 0
+    j = 2*i
+    while j % 5 == 0:
+        j /= 5
+        nz += 1
+    good = sum(2*i - 1) < 2*sum(i)
+    assert good == (d < nz)
+    print mod5(i), mod5(2*i - 1), good, mod5(sum(i)), mod5(sum(2*i - 1)), d, mod5(2*i), d < nz
+print count, count2
--- euler383/euler383_II.py
+++ euler383/euler383_II.py
@@ -1,25 +1,50 @@
 #!/usr/bin/env python
 
+def mod5(i):
+    if 0 != i:
+        return mod5(i/5) + chr(i % 5 + ord('0'))
+    else:
+        return ""
+
 N = 1000000000
-d = 0
+# N = 1000
+s1 = 0
+s2 = 0
 count = 0
 for i in xrange(1, N + 1):
     if i % 1000000 == 0:
         print i, count
     j = i
     while j % 5 == 4:
-        d -= 1
+        s1 += 1
         j /= 5
     j = i
     while j % 5 == 2:
-        d += 1
+        s2 += 1
         j /= 5
     nz = 0
     j = i
     while j % 5 == 0:
         nz += 1
         j /= 5
-    if d < nz:
+    if s2 - s1 < nz:
         count += 1
         # print count, i
+    nbad = 0
+    was = False
+    while j:
+        d = j % 5
+        if not was:
+            if d >= 3:
+                was = True
+                nbad += 1
+        else:
+            if d >= 2:
+                nbad += 1
+            else:
+                was = False
+        j /= 5
+    print mod5(i), s2 - s1, nbad, nz
+    if nz > 0:
+        assert nbad == s2 - s1
 print count
