363d782e5c0b8ebae5052c56745dc2a070198f8e
Den Raskovalov <denplusplus@yandex-team.ru>
2012-05-08 17:21:27 +0400

    xif

--- euler359/euler359.py
+++ euler359/euler359.py
@@ -1,6 +1,50 @@
 #!/usr/bin/env python
 
+import sys
 import math
+
+def hilbert(floor, index):
+    if 0 == floor:
+        index += 1
+    a = (floor + 1)**2 / 2
+    d1 = 1
+    d2 = 4*((floor + 1)/2) + 1
+    a += (d1 + d2)*(index / 2)
+    begin = index - (index & 1)
+    for i in xrange(begin, index):
+        if (floor + 1 + i) & 1 == 0:
+            a += d2
+        else:
+            a += d1
+    a += (index)*(index - 1)/2
+    return a
+
+N0 = 71328803586048
+MOD = 100000000
+N = N0
+n1 = 0
+while N % 2 == 0:
+    n1 += 1
+    N /= 2
+n2 = 0
+while N % 3 == 0:
+    n2 += 1
+    N /= 3
+
+sum = 0
+for i in xrange(n1 + 1):
+    
+    a = 1
+    for i1 in xrange(i):
+        a *= 2
+    
+    for j in xrange(n2 + 1):
+        b = a
+        for j1 in xrange(j):
+            b *= 3
+
+        sum += hilbert(b - 1, N0/b - 1)
+print sum % MOD
 
 def IsSquare(n):
     s = int(math.sqrt(n))
@@ -28,18 +72,6 @@ for j, f in enumerate(floors):
     print
 
 print "----"
-
-def hilbert(floor, index):
-    if 0 == floor:
-        index += 1
-    a = (floor + 1)**2 / 2
-    for i in xrange(index):
-        if (floor + 1 + i) & 1 == 0:
-            a += 4*((floor + 1)/2) + 1
-        else:
-            a += 1
-    a += (index)*(index + 1)/2
-    return a
 
 for j, f in enumerate(floors):
     for i in xrange(len(f)):
