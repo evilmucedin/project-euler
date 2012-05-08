d0e4f2c1754194d622d491c6c04e3f819a7ba728
Den Raskovalov <denplusplus@yandex-team.ru>
2012-05-08 18:49:17 +0400

    fix

--- euler333/euler333.py
+++ euler333/euler333.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python
 
+N = 100
 N = 1000000
 
 ways = [0]*(N + 1)
 nums = []
+left = N + 1
 
 def gen(now, index, cur):
-    if index < 10:
-        print index, now
+    if index < 50:
+        print cur, index, now, left
     if now <= N:
-        if ways[now] == 0:
-            ways[now] = 1
-            for i in xrange(index, len(nums)):
-                j = 0
-                while j < len(cur) and 0 != cur[j] % nums[i]:
-                    j += 1
-                if j == len(cur):
-                    cur.append(nums[i])
-                    gen(now + nums[i], i + 1, cur)
-                    cur.pop()
-        else:
-            ways[now] = 2
+        ways[now] += 1
+        if ways[now] == 1:
+            global left
+            left -= 1
+        # print now, cur
+        for i in xrange(index, len(nums)):
+            j = 0
+            while j < len(cur) and 0 != cur[j] % nums[i] and 0 != nums[i] % cur[j]:
+                j += 1
+            if j == len(cur):
+                cur.append(nums[i])
+                gen(now + nums[i], i + 1, cur)
+                cur.pop()
 
 for i in xrange(1, N + 1):
     j = i
@@ -38,6 +41,7 @@ erato = [True]*N
 for i in xrange(2, N):
     if erato[i]:
         if ways[i] == 1:
+            print "!!", i
             sum += i
         j = i + i
         while j < N:
