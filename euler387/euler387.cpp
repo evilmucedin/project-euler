d6ab7fe94ff4dc0dc830a3cd5a4cdc57731a1cd8
Den Raskovalov <denbox@gmail.com>
2012-10-04 23:10:06 +0400

    solution

--- euler387/euler387.cpp
+++ euler387/euler387.cpp
@@ -1,5 +1,36 @@
 #include <cstdio>
+#include <cmath>
+
+typedef __int64 i64;
+
+i64 sum = 0;
+
+static bool IsPrime(i64 number) {
+    if (number < 2)
+        return false;
+    i64 top = (i64)sqrt((double)number);
+    for (size_t i = 2; i <= top; ++i)
+        if (0 == (number % i))
+            return false;
+    return true;
+}
+
+static void Enum(i64 number, i64 ds, int digits) {
+    if (digits < 14) {
+        if (0 == ds || 0 == (number % ds)) {
+            for (i64 i = 0; i < 10; ++i) {
+                const i64 cand = 10*number + i;
+                if (IsPrime(number/ds) && IsPrime(cand))
+                    sum += cand;
+                Enum(cand, ds + i, digits + 1);
+            }
+        }
+    }
+}
 
 int main() {
+    for (int i = 1; i < 10; ++i)
+        Enum(i, i, 1);
+    printf("%I64d\n", sum);
     return 0;
 }
