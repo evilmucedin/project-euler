4c9983038195fee3b09151a00644ff0ac545372b
Den Raskovalov <denbox@gmail.com>
2012-10-04 23:14:02 +0400

    faster

--- euler387/euler387.cpp
+++ euler387/euler387.cpp
@@ -8,16 +8,22 @@ i64 sum = 0;
 static bool IsPrime(i64 number) {
     if (number < 2)
         return false;
-    i64 top = (i64)sqrt((double)number);
-    for (size_t i = 2; i <= top; ++i)
-        if (0 == (number % i))
-            return false;
-    return true;
+    if (number == 2)
+        return true;
+    if (number & 1) {
+        i64 top = (i64)sqrt((double)number);
+        for (size_t i = 3; i <= top; i += 2)
+            if (0 == (number % i))
+                return false;
+        return true;
+    } else {
+        return false;
+    }
 }
 
 static void Enum(i64 number, i64 ds, int digits) {
     if (digits < 14) {
-        if (0 == ds || 0 == (number % ds)) {
+        if (0 == (number % ds)) {
             for (i64 i = 0; i < 10; ++i) {
                 const i64 cand = 10*number + i;
                 if (IsPrime(number/ds) && IsPrime(cand))
