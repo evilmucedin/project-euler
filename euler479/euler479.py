84e38d3ee86421002d91c649e02feeefb40372b6
Den Raskovalov <denplusplus@yandex-team.ru>
2015-01-02 13:00:17 -0800

    fix

--- euler479/euler479.py
+++ euler479/euler479.py
@@ -13,7 +13,7 @@ sum2 = 0
 for k in xrange(1, n + 1):
     if (k % 10000) == 0:
         print(k)
-    a = common.fastPowerMod(k*k - 1, n, p)
+    a = common.fastPowerMod((k*k - 1) % p, n, p)
     if 0 != (n % 2):
         a = -a
     sum2 += (a - 1)*(k*k - 1)*common.modInv(k*k % p, p)
