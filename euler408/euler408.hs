3083d0e2b708b36ef1496cd53faafdd7771f665d
Den Raskovalov <denplusplus@yandex-team.ru>
2015-01-03 15:51:38 -0800

    408

--- euler408/euler408.hs
+++ euler408/euler408.hs
@@ -1,28 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE PostfixOperators #-}
 
-{- Let's call a lattice point (x, y) inadmissible if x, y and x + y are all
-positive perfect squares. For example, (9, 16) is inadmissible, while (0, 4),
-(3, 1) and (9, 4) are not.
-
-Consider a path from point (x1, y1) to point (x2, y2) using only unit steps
-north or east. Let's call such a path admissible if none of its intermediate
-points are inadmissible.
-
-P(n) be the number of admissible paths from (0, 0) to (n, n).
-It can be verified that P(5) = 252, P(16) = 596994440 and
-P(1000) mod 1 000 000 007 = 341920854.
-
-Find P(10 000 000) mod 1 000 000 007.
-
--------------------------------------
-
-Runs in approximately 1m3s. Compile and run with
-	
-	$ ghc -O2 408.hs; time ./408
-
-Solution by Jack Wines and Brett Wines on December 26, 2014 -}
-
 import qualified Zora.Math as ZM
 import qualified Data.List as List
 import qualified Data.Array.Unboxed as UArray
