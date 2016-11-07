5444fc71fdfc7513f75af018004aa5e091ae9c4f
Den Raskovalov <denbox@gmail.com>
2016-11-06 23:51:16 -0800

    buck

--- euler504/504.cpp
+++ euler504/504.cpp
@@ -10,7 +10,7 @@ int main() {
     vector<vector<uint64_t>> pointsInTriangle(N + 1, vector<uint64_t>(N + 1, 0));
     for (uint64_t x = 1; x <= N; ++x) {
         for (uint64_t y = 1; y <= N; ++y) {
-            uint64_t count = 0;
+            uint64_t& count = pointsInTriangle[x][y];
             for (uint64_t i = 1; i <= x; ++i) {
                 for (uint64_t j = 1; j <= y; ++j) {
                     if (j*x + i*y < x*y) {
@@ -18,7 +18,6 @@ int main() {
                     }
                 }
             }
-            pointsInTriangle[x][y] = count;
         }
     }
 
@@ -28,7 +27,7 @@ int main() {
             for (uint64_t c = 1; c <= N; ++c) {
                 for (uint64_t d = 1; d <= N; ++d) {
                     uint64_t square = pointsInTriangle[a][b] + pointsInTriangle[b][c] + pointsInTriangle[c][d] + pointsInTriangle[d][a] + a + c + b + d - 3;
-                    uint64_t root = sqrt(square);
+                    uint64_t root = sqrt((long double)square);
                     if (square == root*root) {
                         ++count;
                     }
