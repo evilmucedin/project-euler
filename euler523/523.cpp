1447e956749e0523540d452cbbf20629217feefe
Den Raskovalov <denbox@gmail.com>
2016-12-04 21:06:08 -0800

    naive 523

--- euler523/523.cpp
+++ euler523/523.cpp
@@ -1,7 +1,7 @@
 #include "lib/header.h"
 
 int main() {
-    static constexpr size_t kM = 100000;
+    static constexpr size_t kM = 10;
     double ops = 0.0;
 
     static constexpr size_t kN = 30;
@@ -12,7 +12,23 @@ int main() {
 
     for (size_t i = 0; i < kM; ++i) {
         random_shuffle(v.begin(), v.end());
+
+        while (true) {
+            size_t j = 1;
+            while ( j < v.size() && v[j - 1] < v[j] ) {
+                ++j;
+            }
+            if (j == v.size()) {
+                break;
+            }
+            auto temp = v[j];
+            v.erase(v.begin() + j);
+            v.insert(v.begin(), temp);
+            ++ops;
+        }
     }
+
+    cout << ops / kM << endl;
 
     return 0;
 }
