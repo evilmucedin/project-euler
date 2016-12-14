2b481603ee36eeabaca25f6f4f974c272f1aad92
Den Raskovalov <denbox@gmail.com>
2016-12-13 21:55:59 -0800

    grad

--- euler262py/euler262.py
+++ euler262py/euler262.py
@@ -4,7 +4,10 @@ import theano
 
 x = theano.tensor.dscalar()
 y = theano.tensor.dscalar()
-h = theano.function([x, y], (5000 - 0.005*(x*x+y*y+x*y) + 12.5*(x+y))*theano.tensor.exp( -abs(0.000001*(x*x+y*y)-0.0015*(x+y)+0.7) ))
+h = (5000 - 0.005*(x*x+y*y+x*y) + 12.5*(x+y))*theano.tensor.exp( -abs(0.000001*(x*x+y*y)-0.0015*(x+y)+0.7) )
+fh = theano.function([x, y], h)
 gradH = theano.gradient.grad(h, [x, y])
-print h(0, 0), gradH[0](0, 0), gradH[1](0, 0)
+gradHX = theano.function([x, y], gradH[0])
+gradHY = theano.function([x, y], gradH[1])
+print fh(0, 0), gradHX(0, 0), gradHY(0, 0)
 
