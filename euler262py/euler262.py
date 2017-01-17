7fd378c3c3d3dcfd2c330c61d150f48d5de854e5
Den Raskovalov <denbox@gmail.com>
2017-01-17 00:06:04 -0800

    cleaner output

--- euler262py/euler262.py
+++ euler262py/euler262.py
@@ -4,6 +4,8 @@ import math
 import theano
 from scipy.integrate import quad
 
+DEBUG = 0
+
 def norm(v):
     return math.sqrt(v[0]*v[0] + v[1]*v[1])
 
@@ -25,11 +27,6 @@ def curvLength(f, left, right):
         y = yNext
     return result
 
-def circle(x):
-    if x < -1 or x > 1:
-        return 0
-    return math.sqrt(1.0 - x*x)
-
 def sqr(x):
     return x*x
 
@@ -41,16 +38,22 @@ def deriv(f, x):
 def fLength(f, x):
     return math.sqrt(1.0 + sqr(deriv(f, x)))
 
-l = curvLength(circle, -1.0, 1.0)
-print("curvLength test: ", l, math.pi - l)
-q = quad(lambda x: fLength(circle, x), -1.0, 1.0)
-print(q, math.pi - q[0])
+if DEBUG:
+    def circle(x):
+        if x < -1 or x > 1:
+            return 0
+        return math.sqrt(1.0 - x*x)
 
-def one(x):
-    return 1
+    l = curvLength(circle, -1.0, 1.0)
+    print("curvLength test: ", l, math.pi - l)
+    q = quad(lambda x: fLength(circle, x), -1.0, 1.0)
+    print(q, math.pi - q[0])
 
-print("curvLength test: ", curvLength(one, 0.0, 1.0))
-print(quad(lambda x: fLength(one, x), 0.0, 1.0))
+    def one(x):
+        return 1
+
+    print("curvLength test: ", curvLength(one, 0.0, 1.0))
+    print(quad(lambda x: fLength(one, x), 0.0, 1.0))
 
 x = theano.tensor.dscalar()
 y = theano.tensor.dscalar()
@@ -59,7 +62,8 @@ fh = theano.function([x, y], h)
 gradH = theano.gradient.grad(h, [x, y])
 gradHX = theano.function([x, y], gradH[0])
 gradHY = theano.function([x, y], gradH[1])
-print(fh(0, 0), gradHX(0, 0), gradHY(0, 0), x.dtype)
+if DEBUG:
+    print(fh(0, 0), gradHX(0, 0), gradHY(0, 0), x.dtype)
 fhX0 = theano.function([y, theano.In(x, value=0)], h)
 gradHX0Y = theano.function([y, theano.In(x, value=0)], gradH[1])
 
@@ -115,8 +119,9 @@ def fUp(x):
 def fDown(x):
     return findRoot3(fhDiff, x, y0, 1600)
 
-print(fUp(100))
-print(fDown(873))
+if DEBUG:
+    print(fUp(100))
+    print(fDown(873))
 
 start = (200, 200)
 finish = (1400, 1400)
@@ -148,7 +153,8 @@ def searchMax(f, left, right):
                 argMax = x
         left = argMax - 3*delta
         right = argMax + 3*delta
-    print("searchMax %lf %lf %lf" % (right, left, deriv(f, argMax)))
+    if DEBUG:
+        print("searchMax %lf %lf %lf" % (right, left, deriv(f, argMax)))
     return argMax
 
 def fUpFinish(x):
@@ -161,7 +167,8 @@ t1 = (x1, fUp(x1))
 def fDownFinish(x):
     return orth(fDown, x, finish)
 
-print("fDownFinish: %f" % fDownFinish(873))
+if DEBUG:
+    print("fDownFinish: %f" % fDownFinish(873))
 
 x2 = searchMax(fDownFinish, 400.0, 1000.0)
 print("tan2", x2, fDownFinish(x2))
@@ -176,9 +183,10 @@ result = s1 + s2 + s3 + s4
 print("%.20lf" % result)
 print("%.3lf" % result)
 
-s2 = quad(lambda x: fLength(fUp, x), 0, x1)
-s3 = quad(lambda x: fLength(fDown, x), 0, x2)
-print(s1, s2, s3, s4)
-result = s1 + s2[0] + s3[0] + s4
-print("%.20lf" % result)
-print("%.3lf" % result)
+if DEBUG:
+    s2 = quad(lambda x: fLength(fUp, x), 0, x1)
+    s3 = quad(lambda x: fLength(fDown, x), 0, x2)
+    print(s1, s2, s3, s4)
+    result = s1 + s2[0] + s3[0] + s4
+    print("%.20lf" % result)
+    print("%.3lf" % result)
