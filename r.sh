e528d4f5152bafa337c501bf3550a4f9f346a475
Den Raskovalov <denplusplus@fb.com>
2017-01-28 20:37:06 -0800

    a fix for Darwin

--- r.sh
+++ r.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 
-buck build @mode/opt euler$1/... 
+unamestr=`uname`
+
+buck_bin=buck
+
+if [ "${unamestr}" = "Darwin" ]; then
+    buck_bin=~/Temp/buck/bin/buck
+fi
+
+${buck_bin} build @mode/opt euler$1/...
 res=$?
 if [ 0 -eq ${res} ]; then
     buck-out/gen/euler$1/$1
