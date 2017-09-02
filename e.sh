e348f72d00241dc7f11e4fd366bef7851d447bd1
Den Raskovalov <denbox@gmail.com>
2017-09-02 10:52:25 -0700

    change edit script

--- e.sh
+++ e.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
-filename=euler$1/$1.cpp
+dir=euler$1
+if [ ! -e $dir ]; then
+    echo "$dir not found"
+    exit 1
+fi
+
+pushd .
+cd $dir
+
+filename=$1.cpp
 if [ -e $filename ]; then
-    vim -p $filename euler$1/BUCK ${@:2}
+    vim -p $filename BUCK ${@:2}
 else
     echo "$filename not found"
     exit 1
 fi
+
+popd
