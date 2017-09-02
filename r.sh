c1bc05090d237abed9d8c5957fd64e475b81cce0
Den Raskovalov <denbox@gmail.com>
2017-09-02 11:38:41 -0700

    inline vim compilation

--- r.sh
+++ r.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env sh
 
-unamestr=`uname`
-
-buck_bin=buck
-
-if [ "${unamestr}" = "Darwin" ]; then
-    buck_bin=~/Temp/buck/bin/buck
-fi
+. cCommon.sh
 
 ${buck_bin} build @mode/opt euler$1/...
 res=$?
