e97e6c571d7300b7a89ab8d0615b842a9dee82e7
Den Raskovalov <denbox@gmail.com>
2017-10-28 05:04:23 -0700

    random

---
+++ lib/random.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <random>
+
+std::mt19937_64& getRandomNumberGenerator() {
+    thread_local std::mt19937_64 rng;
+    return rng;
+}
+
+template<typename T>
+T rand01() {
+    static std::uniform_real_distribution<T> unif(0, 1);
+    return unif(getRandomNumberGenerator());
+}
+
+template<typename T>
+T rand1() {
+    static std::uniform_real_distribution<T> unif(-1, 1);
+    return unif(getRandomNumberGenerator());
+}
+
+template<typename T>
+T dice(T n) {
+    return getRandomNumberGenerator()() % n;
+}
+
+template<typename T>
+bool oneIn(T n) {
+    return 0 == dice(n);
+}
