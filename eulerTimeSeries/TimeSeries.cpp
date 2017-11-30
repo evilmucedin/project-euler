133725ef945fdbf11648f461a0d5519ea8cecffb
Den Raskovalov <denbox@gmail.com>
2017-11-30 02:20:36 -0800

    fixes

--- eulerTimeSeries/TimeSeries.cpp
+++ eulerTimeSeries/TimeSeries.cpp
@@ -3,17 +3,19 @@
 #include "lib/init.h"
 #include "lib/timer.h"
 #include "lib/io/zstream.h"
+#include "lib/io/csv.h"
 
 int main(int argc, char* argv[]) {
     Timer tTotal("Total");
     standardInit(argc, argv);
     auto fIn = make_shared<ifstream>("/home/denplusplus/Downloads/NSQ-2017-11-28-MARKETPRICE-Data-1-of-1.csv.gz", std::ifstream::binary);
     auto zIn = make_shared<ZIStream>(fIn);
-    string line;
+    CsvParser reader(zIn);
+    reader.readHeader();
     int iLine = 0;
-    while (getline(*zIn, line)) {
+    while (reader.readLine()) {
         if (0 == (iLine % 100000000)) {
-            LOG(INFO) << OUT(iLine);
+            LOG(INFO) << OUT(iLine) << OUT(reader.size());
         }
         ++iLine;
     }
