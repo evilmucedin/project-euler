ee5671c96e4bd869d37c4f90b68104c4cee26d25
Den Raskovalov <denbox@gmail.com>
2017-11-30 14:33:51 -0800

    ofstream

---
+++ lib/io/fstream.cpp
@@ -0,0 +1,91 @@
+#include "lib/io/fstream.h"
+
+#include <cstring>
+
+namespace {
+string strError() {
+  string buff(80, '\0');
+  auto p = strerror_r(errno, const_cast<char*>(buff.data()), buff.size());
+  return string(p, strlen(p));
+}
+
+string modeToString(ios_base::openmode mode) {
+  static constexpr array<ios_base::openmode, 6> kModeValV{
+      ios_base::in,  ios_base::out,   ios_base::app,
+      ios_base::ate, ios_base::trunc, ios_base::binary};
+
+  static const array<string, kModeValV.size()> kModeNameV{
+      "in", "out", "app", "ate", "trunc", "binary"};
+
+  string res;
+  for (size_t i = 0; i < kModeValV.size(); ++i) {
+    if (mode & kModeValV[i]) {
+      res += (!res.empty()) ? "|" : "";
+      res += kModeNameV[i];
+    }
+  }
+  if (res.empty()) {
+    res = "none";
+  }
+  return res;
+}
+
+void checkMode(const string& filename, ios_base::openmode mode) {
+  if ((mode & ios_base::trunc) && !(mode & ios_base::out)) {
+    throw Exception(string("strict_fstream: open('") + filename +
+                    "'): mode error: trunc and not out");
+  } else if ((mode & ios_base::app) && !(mode & ios_base::out)) {
+    throw Exception(string("strict_fstream: open('") + filename +
+                    "'): mode error: app and not out");
+  } else if ((mode & ios_base::trunc) && (mode & ios_base::app)) {
+    throw Exception(string("strict_fstream: open('") + filename +
+                    "'): mode error: trunc and app");
+  }
+}
+
+void checkOpen(ios* s_p, const string& filename, ios_base::openmode mode) {
+  if (s_p->fail()) {
+    throw Exception(string("strict_fstream: open('") + filename + "'," +
+                    modeToString(mode) + "): open failed: " + strError());
+  }
+}
+
+void checkPeek(istream* is_p, const string& filename, ios_base::openmode mode) {
+  bool peekFailed = true;
+  try {
+    is_p->peek();
+    peekFailed = is_p->fail();
+  } catch (ios_base::failure e) {
+  }
+  if (peekFailed) {
+    throw Exception(string("strict_fstream: open('") + filename + "'," +
+                    modeToString(mode) + "): peek failed: " + strError());
+  }
+  is_p->clear();
+}
+}
+
+IFstream::IFstream(const string& filename, ios_base::openmode mode) {
+  open(filename, mode);
+}
+
+void IFstream::open(const string& filename, ios_base::openmode mode) {
+  mode |= ios_base::in;
+  exceptions(ios_base::badbit);
+  checkMode(filename, mode);
+  ifstream::open(filename, mode);
+  checkOpen(this, filename, mode);
+  checkPeek(this, filename, mode);
+}
+
+OFstream::OFstream(const string& filename, ios_base::openmode mode) {
+  open(filename, mode);
+}
+
+void OFstream::open(const string& filename, ios_base::openmode mode) {
+  mode |= ios_base::out;
+  exceptions(ios_base::badbit);
+  checkMode(filename, mode);
+  ofstream::open(filename, mode);
+  checkOpen(this, filename, mode);
+}
---
+++ lib/io/fstream.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "lib/header.h"
+
+class IFstream : public ifstream {
+ public:
+  IFstream() = default;
+
+  IFstream(const string& filename, ios_base::openmode mode = ios_base::in);
+  void open(const string& filename, ios_base::openmode mode = ios_base::in);
+};
+
+class OFstream : public ofstream {
+ public:
+  OFstream() = default;
+
+  OFstream(const string& filename, ios_base::openmode mode = ios_base::out);
+  void open(const string& filename, ios_base::openmode mode = ios_base::out);
+};
