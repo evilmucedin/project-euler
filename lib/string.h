a4fe4dbec5f57d76f9f8c3dbb4df870fa5eb3bf2
Den Raskovalov <denbox@gmail.com>
2017-12-01 00:56:57 -0800

    generic split

--- lib/string.h
+++ lib/string.h
@@ -3,3 +3,34 @@
 #include "lib/header.h"
 
 StringVector split(const string& s, char delim = '\t');
+
+template <typename T>
+StringVector splitByFunctor(const string& s, T isDelimiter) {
+    StringVector result;
+    auto p = s.data();
+    auto begin = p;
+    bool in = true;
+    while (*p) {
+        bool delim = isDelimiter(*p);
+        if (in) {
+            if (delim) {
+                if (p != begin) {
+                    result.emplace_back(begin, p - begin);
+                }
+                in = false;
+            }
+        } else {
+            if (!delim) {
+                begin = p;
+                in = true;
+            }
+        }
+        ++p;
+    }
+    if (in) {
+        if (p != begin) {
+            result.emplace_back(begin, p - begin);
+        }
+    }
+    return result;
+}
--- lib/tests/stringTest.cpp
+++ lib/tests/stringTest.cpp
@@ -1,5 +1,7 @@
 #include "lib/string.h"
 
+#include <cctype>
+
 #include "gtest/gtest.h"
 
 TEST(Split, Empty) { EXPECT_TRUE(split("") == (StringVector){}); }
@@ -11,3 +13,9 @@ TEST(Split, Two) { EXPECT_TRUE(split("a,b", ',') == ((StringVector){"a", "b"}));
 TEST(Split, Three) { EXPECT_TRUE(split("a,,b", ',') == ((StringVector){"a", "", "b"})); }
 
 TEST(Split, Three2) { EXPECT_TRUE(split(",a,b", ',') == ((StringVector){"", "a", "b"})); }
+
+TEST(Functor, Simple) {
+    auto splitResult = splitByFunctor("0aaa111bb3", [](char ch) { return isdigit(ch); });
+    cerr << splitResult << endl;
+    EXPECT_TRUE(splitResult == ((StringVector){"aaa", "bb"}));
+}
