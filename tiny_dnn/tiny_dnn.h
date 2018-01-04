1d483819421cbb1d55afc382ef31edd472dce672
Den Raskovalov <denbox@gmail.com>
2018-01-03 18:54:31 -0800

    separate io

--- tiny_dnn/tiny_dnn.h
+++ tiny_dnn/tiny_dnn.h
@@ -63,15 +63,6 @@
 #include "tiny_dnn/util/product.h"
 #include "tiny_dnn/util/weight_init.h"
 
-#include "tiny_dnn/io/cifar10_parser.h"
-#include "tiny_dnn/io/display.h"
-#include "tiny_dnn/io/layer_factory.h"
-#include "tiny_dnn/io/mnist_parser.h"
-
-#ifdef DNN_USE_IMAGE_API
-#include "tiny_dnn/util/image.h"
-#endif  // DNN_USE_IMAGE_API
-
 #ifndef CNN_NO_SERIALIZATION
 #include "tiny_dnn/util/deserialization_helper.h"
 #include "tiny_dnn/util/serialization_helper.h"
@@ -164,8 +155,3 @@ using slice = tiny_dnn::slice_layer;
 using power = tiny_dnn::power_layer;
 
 }  // namespace tiny_dnn
-
-#ifdef CNN_USE_CAFFE_CONVERTER
-// experimental / require google protobuf
-#include "tiny_dnn/io/caffe/layer_factory.h"
-#endif
