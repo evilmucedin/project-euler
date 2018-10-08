d51421bcc6cb5d681bb2f08a72dd5ee047c39608
Den Raskovalov <denbox@gmail.com>
2018-10-08 01:34:34 -0700

    linear

--- eulerMnist/Mnist.cpp
+++ eulerMnist/Mnist.cpp
@@ -1,11 +1,12 @@
 #include "lib/header.h"
 
+#include "glog/logging.h"
+
 #include "lib/init.h"
 #include "lib/io/csv.h"
 #include "lib/io/fstream.h"
 #include "lib/io/zstream.h"
-
-#include "glog/logging.h"
+#include "lib/matrix.h"
 
 struct Row {
     int label_;
@@ -17,7 +18,9 @@ struct Data {
     Rows rows;
 };
 
-Rows readRows(const std::string& filename) {
+static constexpr size_t N_FEATURES = 784;
+
+Data readRows(const std::string& filename) {
     auto fstream = std::make_shared<IFStream>(filename);
     auto zfstream = std::make_shared<ZIStream>(fstream);
     auto csv = std::make_shared<CsvParser>(zfstream);
@@ -25,7 +28,7 @@ Rows readRows(const std::string& filename) {
     Rows rows;
     auto labelIndex = csv->getIndexOrDie("label");
     std::vector<int> pixelIndices;
-    for (size_t i = 0; i < 784; ++i) {
+    for (size_t i = 0; i < N_FEATURES; ++i) {
         pixelIndices.emplace_back(csv->getIndexOrDie("pixel" + std::to_string(i)));
     }
     while (csv->readLine()) {
@@ -37,11 +40,56 @@ Rows readRows(const std::string& filename) {
         }
         rows.emplace_back(std::move(row));
     }
-    return rows;
+    return {rows};
+}
+
+class LinearRegressionClassifier {
+   public:
+    LinearRegressionClassifier() : xtx_(N_FEATURES), xty_((u32)N_FEATURES) {}
+
+    void addSample(float y, const FloatVector& features) {
+        assert(features.size() == N_FEATURES);
+        for (size_t i = 0; i < features.size(); ++i) {
+            for (size_t j = 0; j < features.size(); ++j) {
+                xtx_.data_[i][j] += features[i] * features[j];
+            }
+        }
+        for (size_t i = 0; i < features.size(); ++i) {
+            xty_[i] += features[i] * y;
+        }
+
+    }
+
+    void train() {
+        inv_ = xtx_.invert();
+        bPrime_ = inv_ * xty_;
+    }
+
+   private:
+    Matrix xtx_;
+    Vector xty_;
+    Matrix inv_;
+    Vector bPrime_;
+};
+
+template <typename T>
+vector<T> train(const Data& data) {
+    vector<T> result(10);
+    for (const auto& row : data.rows) {
+        for (size_t i = 0; i < 10; ++i) {
+            result[i].addSample(row.label_ == i, row.features_);
+        }
+    }
+    for (auto& c : result) {
+        c.train();
+    }
+    return result;
 }
 
 int main(int argc, char* argv[]) {
     standardInit(argc, argv);
-    LOG(INFO) << OUT(readRows("train.gz").size());
+    auto trainData = readRows("train.gz");
+    LOG(INFO) << OUT(trainData.rows.size());
+    auto linear = train<LinearRegressionClassifier>(trainData);
     return 0;
 }
