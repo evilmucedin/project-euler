4a16b96ec2dd46c4343e23492fd82f6037a9aecd
Den Raskovalov <denbox@gmail.com>
2019-05-04 11:54:57 -0700

    test for RMQ

--- lib/rmq.h
+++ lib/rmq.h
@@ -2,6 +2,8 @@
 
 #include "lib/header.h"
 
+#include <type_traits>
+
 template <typename T>
 size_t logCeil(T n);
 
@@ -15,19 +17,56 @@ inline size_t logCeil<u64>(u64 n) {
     return 64 - __builtin_clzl(n);
 }
 
-template <typename T, typename F>
-class RangeMQuery {
+template <typename T>
+struct MinF {
+    bool operator()(const T& a, const T& b) { return a < b; }
+};
+
+template <typename T>
+struct MaxF {
+    bool operator()(const T& a, const T& b) { return a > b; }
+};
+
+template <typename It, typename F>
+class RangeAggQuery {
+   private:
+    using T = typename std::remove_reference<It>::type;
+
+    static It cmp(It a, It b) {
+        if (F{}(*a, *b)) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
    public:
-    template <typename It>
-    RangeMQuery(It begin, It end) {
+    RangeAggQuery(It begin, It end) : begin_(begin), end_(end) {
         const size_t n = end - begin;
         const size_t nLevels = std::max<size_t>(1, logCeil(n));
         agg_.resize(nLevels);
+        for (It i = begin; i != end; ++i) {
+            agg_[0][i - begin] = i;
+        }
+        for (size_t level = 1; level < nLevels; ++level) {
+            size_t gap = 1ULL << (level - 1);
+            for (It i = begin; i + gap < end; ++i) {
+                agg_[level][i - begin] = cmp(agg_[level - 1][i - begin], agg_[level - 1][i + gap - begin]);
+            }
+        }
+    }
+
+    It agg(It begin, It end) const {
+        size_t diff = end - begin;
+        size_t level = std::log2(diff);
+        return cmp(agg_[level][begin - begin_], agg_[level][end - (1ULL << level) - begin_]) ;
     }
 
    private:
-    using VectorT = std::vector<T>;
-    using VectorVectorT = std::vector<VectorT>;
+    using VectorIt = std::vector<It>;
+    using VectorVectorIt = std::vector<VectorIt>;
 
-    VectorVectorT agg_;
+    It begin_;
+    It end_;
+    VectorVectorIt agg_;
 };
--- lib/tests/rmqTest.cpp
+++ lib/tests/rmqTest.cpp
@@ -16,3 +16,15 @@ TEST(RMQ, LogCeil) {
     EXPECT_EQ(11, logCeil<u32>(1024));
     EXPECT_EQ(11, logCeil<u64>(1024));
 }
+
+TEST(RMQ, Test1) {
+    static constexpr size_t kN = 10000;
+    using SizeTVector = std::vector<size_t>;
+    SizeTVector v(kN);
+    for (size_t i = 0; i < kN; ++i) {
+        v[i] = i;
+    }
+    RangeAggQuery<SizeTVector::iterator, MinF<size_t>> rmq(v.begin(), v.end());
+    EXPECT_EQ(*rmq.agg(v.begin(), v.end()), 0);
+    EXPECT_EQ(*rmq.agg(v.begin(), v.begin() + 1), 0);
+}
