34384a8876ef2524d7d7fd42b303e98b81e2f295
Den Raskovalov <denbox@gmail.com>
2019-06-16 16:40:04 -0700

    finish treap implementation

--- lib/tests/treapTest.cpp
+++ lib/tests/treapTest.cpp
@@ -4,5 +4,19 @@
 
 TEST(Treap, Simple) {
     Treap<int, int> t;
+    EXPECT_EQ(t.size(), 0);
     t.emplace(1, 2);
+    EXPECT_EQ(t.size(), 1);
+    t.emplace(-1, 3);
+    EXPECT_EQ(t.size(), 2);
+    t.emplace(4, 4);
+    EXPECT_EQ(t.size(), 3);
+    EXPECT_EQ(t.get(1), 2);
+    EXPECT_EQ(t.get(-1), 3);
+    EXPECT_EQ(t.get(4), 4);
+    EXPECT_EQ(t.size(), 3);
+    t.erase(-1);
+    t.erase(1);
+    t.erase(4);
+    EXPECT_EQ(t.size(), 0);
 }
--- lib/treap.h
+++ lib/treap.h
@@ -1,9 +1,56 @@
 #pragma once
 
+#include "lib/header.h"
+
 #include "lib/random.h"
 
 template <typename K, typename V>
 class Treap {
+public:
+    void emplace(const K& key, const V& value) {
+        auto splitResult = split(root_, key);
+        auto newNode = allocate(key, value);
+        auto m1 = merge(splitResult.first, newNode);
+        root_ = merge(m1, splitResult.second);
+    }
+
+    size_t size() const {
+        return size(root_);
+    }
+
+    void erase(const K& key) {
+        auto splitResult = split(root_, key);
+        Node* now = splitResult.second;
+        Node* prev = nullptr;
+        Node* prevprev = nullptr;
+        while (now) {
+            prevprev = prev;
+            prev = now;
+            now = now->left_;
+        }
+        assert(prev->key_ == key);
+        if (prevprev) {
+            prevprev->left_ = nullptr;
+        } else {
+            splitResult.second = nullptr;
+        }
+        delete prev;
+        root_ = merge(splitResult.first, splitResult.second);
+    }
+
+    V get(const K& key) {
+        auto now = root_;
+        while (now->key_ != key) {
+            if (now->key_ < key) {
+                now = now->left_;
+            } else {
+                now = now->right_;
+            }
+        }
+        return now->value_;
+    }
+
+   private:
     struct Node {
         K key_;
         V value_;
@@ -12,19 +59,10 @@ class Treap {
         Node* right_;
     };
 
-    void emplace(const K& key, const V& value) {
-        Node* t1;
-        Node* t2;
-        std::tie(t1, t2) = split(root_, key);
-        auto newNode = allocate(key, value);
-
-    }
-
-   private:
     Node* allocate(const K& key, const V& value) {
         auto result = new Node();
         result->key_ = key;
-        result->value = value;
+        result->value_ = value;
         result->priority_ = randInt();
         result->left_ = nullptr;
         result->right_ = nullptr;
@@ -36,11 +74,11 @@ class Treap {
             return {nullptr, nullptr};
         }
         if (n->key_ > key) {
-            auto splitRight = split(n->right, key);
-            n->right = splitRight->first;
-            return {n, splitRight->second};
+            auto splitRight = split(n->right_, key);
+            n->right_ = splitRight.first;
+            return {n, splitRight.second};
         } else {
-            auto splitLeft = split(n->left, key);
+            auto splitLeft = split(n->left_, key);
             n->left_ = splitLeft.second;
             return {splitLeft.first, n};
         }
@@ -53,14 +91,21 @@ class Treap {
         if (!t1) {
             return t2;
         }
-        if (t1->priority_ > t2.priority_) {
+        if (t1->priority_ > t2->priority_) {
             t1->right_ = merge(t1->right_, t2);
             return t1;
         } else {
-            t2->left_ = merge(t1, t2->right_);
+            t2->left_ = merge(t1, t2->left_);
             return t2;
         }
     }
 
-    Node* root_;
+    size_t size(const Node* n) const {
+        if (!n) {
+            return 0;
+        }
+        return 1 + size(n->left_) + size(n->right_);
+    }
+
+    Node* root_{};
 };
