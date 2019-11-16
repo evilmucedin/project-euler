f65959e0905355bbf5aea81d79f4318d38cc753e
Den Raskovalov <denplusplus@yandex-team.ru>
2019-11-16 01:22:45 -0800

    one step closer to solution of 668

--- euler668/668.cpp
+++ euler668/668.cpp
@@ -4,20 +4,40 @@
 
 #include "glog/logging.h"
 
-static constexpr u64 N = 10000000000ULL;
+// static constexpr u64 N = 10000000000ULL;
+static constexpr u64 N = 1000000000ULL;
 
 int main() {
-    Erato erato(sqrt(N) + 10);
-    u64 result = 0;
+    Erato erato(N + 10);
+
+    u64 result = N;
+
+    u64 top = sqrt(N);
+    u64 i = 0;
+    while (erato.primes_[i] <= N) {
+        // LOG(INFO) << OUT(erato.primes_[i]);
+        if (erato.primes_[i] <= top) {
+            result -= erato.primes_[i];
+        } else {
+            result -= N / erato.primes_[i];
+        }
+        ++i;
+    }
+
+    LOG(INFO) << OUT(result);
+
+    /*
+    u64 result2 = 0;
     for (u64 i = 2; i <= N; ++i) {
         auto f = factorization(i, erato);
         if (f.back().factor_ < sqrt(i)) {
-            ++result;
+            ++result2;
         }
-        LOG_EVERY_MS(INFO, 1000) << i << " " << result;
+        // LOG_EVERY_MS(INFO, 1000) << i << " " << result;
     }
 
-    LOG(INFO) << OUT(result + 1);
+    LOG(INFO) << OUT(result2 + 1);
+    */
 
     return 0;
 }
