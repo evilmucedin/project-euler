9356cc1bddd4533de3e01fb22b7bb87c226b8829
Den Raskovalov <denbox@gmail.com>
2020-10-23 01:39:08 -0700

    add rbtree and test

--- lib/tests/redBlackTreeTest.cpp
+++ lib/tests/redBlackTreeTest.cpp
@@ -4,4 +4,9 @@
 
 TEST(RedBlackTree, Sum) {
     static constexpr int kSize = 12345;
+    RedBlackTree<int> t;
+    cout << t.searchTree(1) << endl;
+    t.insert(2);
+    cout << t.searchTree(2)->data << endl;
+    cout << t.searchTree(1) << endl;
 }
