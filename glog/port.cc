5ed17832491d8c71e40e56d1c1df3c0cf3b27e71
Den Raskovalov <denplusplus@yandex-team.ru>
2021-02-21 22:12:23 -0800

    fix unix build

--- glog/port.cc
+++ glog/port.cc
@@ -1,10 +1,10 @@
 /* Copyright (c) 2008, Google Inc.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above
@@ -14,7 +14,7 @@
  *     * Neither the name of Google Inc. nor the names of its
  * contributors may be used to endorse or promote products derived from
  * this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -32,9 +32,7 @@
  * Copied from google-perftools and modified by Shinichiro Hamaji
  */
 
-#ifndef _WIN32
-# error You should only be including windows/port.cc in a windows environment!
-#endif
+#ifdef _WIN32
 
 #include "config.h"
 #include <stdarg.h>    // for va_list, va_start, va_end
@@ -68,4 +66,6 @@ int snprintf(char *str, size_t size, const char *format, ...) {
   va_end(ap);
   return r;
 }
+#endif
+
 #endif
