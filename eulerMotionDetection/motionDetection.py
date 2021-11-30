f98b1d55924fbb85e007752eaf419bc305d49a41
U-REDMOND\deraskov <denplusplus@yandex-team.ru>
2021-11-30 14:40:12 -0800

    run for sitting and running

--- eulerMotionDetection/motionDetection.py
+++ eulerMotionDetection/motionDetection.py
@@ -4,17 +4,21 @@ import pandas
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pandas.read_csv("s01.txt", header=None)
-print(data.head())
+for a in ["sitting", "running"]:
+	data = pandas.read_csv("%s.txt" % a, header=None)
+	print(data.head())
 
-figure, ax1 = plt.subplots()
-ax1.plot(data[0])
-plt.show()
+	figure, ax1 = plt.subplots()
+	ax1.plot(data[0])
+	ax1.plot(data[1])
+	ax1.plot(data[2])
+	figure.savefig("%sData.png" % a)
+	plt.show()
 
-plt.clf()
-plt.cla()
-figure, ax1 = plt.subplots()
-freq = np.fft.fft(data[0])
-ax1.plot(freq.real)
-figure.savefig("fft.png")
-plt.show()
+	plt.clf()
+	plt.cla()
+	figure, ax1 = plt.subplots()
+	freq = np.fft.fft(data[0])
+	ax1.plot(freq.real[1:])
+	figure.savefig("%sFFT.png" % a)
+	plt.show()
