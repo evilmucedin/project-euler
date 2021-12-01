44f623e85fd85ad94550716c8a2106aa25a1da95
Den Raskovalov <denplusplus@yandex-team.ru>
2021-11-30 23:05:19 -0800

    first Rust program

--- advent/2021/1rust/1.rs
+++ advent/2021/1rust/1.rs
@@ -1,3 +1,24 @@
+fn get_input() -> String {
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer).expect("Failed");
+    buffer
+}
+
 fn main() {
-    println!("Hello world")
+    let mut count = 0;
+    let mut n = get_input().trim().parse::<i32>().unwrap();
+    loop {
+        match get_input().trim().parse::<i32>() {
+            Ok(nn) => {
+                if nn > n {
+                    count += 1;
+                }
+                n = nn;
+            },
+            Err(_) => {
+                break;
+            }
+        }
+    }
+    println!("{}", count);
 }
