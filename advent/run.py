c7b00cb130ec556cfcd03d339e333c7e8a40325e
Den Raskovalov <denplusplus@yandex-team.ru>
2021-12-02 22:36:02 -0800

    option to override input

--- advent/run.py
+++ advent/run.py
@@ -4,8 +4,19 @@ import os
 import subprocess
 import requests
 
+import argparse
+
 def main():
     YEAR = 2021
+
+    parser = argparse.ArgumentParser(
+        description="Run Advent of Code solutions")
+    parser.add_argument("--task", type=int,
+                        help="task#", default=1)
+    parser.add_argument("--input", type=str,
+                        help="input file", default="")
+
+    args = parser.parse_args()
 
     subprocess.check_output(["buck", "build", "@mode/opt", "..."])
 
@@ -15,13 +26,17 @@ def main():
     cookies = {"session": "53616c7465645f5f396f77dccd06e249b33e25731f82cc9c72bc1bb12a51e8462e1dcb216cb73e67f06c9b1e3c770b1a"}
 
     def test(testFilename, testNumber):
-        url = f"https://adventofcode.com/{YEAR}/day/{task}/input"
-        with open(testFilename, "wb") as fOut:
-            fOut.write(requests.get(url, allow_redirects=True, cookies=cookies).content)
+        if not args.input:
+            url = f"https://adventofcode.com/{YEAR}/day/{task}/input"
+            with open(testFilename, "wb") as fOut:
+                fOut.write(requests.get(url, allow_redirects=True, cookies=cookies).content)
         subprocess.check_call(f"{bn} --test={testNumber}", shell=True, stderr=subprocess.STDOUT, stdin=open(testFilename))
 
-    test("first.in", 1)
-    test("second.in", 2)
+    if args.input:
+        test(args.input, args.task)
+    else:
+        test("first.in", 1)
+        test("second.in", 2)
 
 if __name__ == "__main__":
     main()
