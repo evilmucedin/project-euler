130600358c385d442633357bb18e90bb3e8f72d8
Den Raskovalov <denplusplus@yandex-team.ru>
2021-12-19 13:23:37 -0800

    small optimization of 19

--- advent/2021/19/19.cpp
+++ advent/2021/19/19.cpp
@@ -70,8 +70,10 @@ size_t setIntersectionSize(InputIterator1 first1, InputIterator1 last1, InputIte
     return result;
 }
 
-bool findOperator(const PointSet& s0, const vector<Point>& data0, const vector<Point> data, Operator& o) {
+bool findOperator(const PointSet& s0, const Points& data0, const Points& data, Operator& o) {
     size_t maxCount = 0;
+    Points dataRotated(data.size());
+
     for (int perm1 = 0; perm1 < 3; ++perm1) {
         for (int perm2 = 0; perm2 < 3; ++perm2) {
             if (perm1 == perm2) {
@@ -88,23 +90,29 @@ bool findOperator(const PointSet& s0, const vector<Point>& data0, const vector<P
                         for (int sign3 = -1; sign3 < 2; sign3 += 2) {
                             o.sign = {sign1, sign2, sign3};
 
-                            for (int i = 0; i < data0.size(); ++i) {
-                                const auto& p0 = data0[i];
+                            o.move = Point::ZERO;
+                            REP(i, data.size()) {
+                                dataRotated[i] = o.apply(data[i]);
+                            }
 
+                            for (int i = 0; i < data0.size(); ++i) {
                                 for (int j = 0; j < data.size(); ++j) {
-                                    o.move = Point::ZERO;
-                                    const auto& p = o.apply(data[j]);
+                                    o.move = data0[i] - dataRotated[j];
                                     // cerr << o.move << endl;
-                                    o.move = p0 - p;
 
                                     // cerr << p0 << ", " << data[j] << ", " << o.apply(data[j]) << endl;
 
                                     size_t count = 0;
-                                    for (const auto& p : data) {
-                                        if (s0.count(o.apply(p))) {
+                                    REP (k, data.size()) {
+                                        if (s0.count(dataRotated[k] + o.move)) {
                                             ++count;
                                         }
                                     }
+
+                                    if (count >= 12) {
+                                        return true;
+                                    }
+
                                     // cerr << o.move << " " << count << endl;
                                     if (count > maxCount) {
                                         maxCount = count;
@@ -114,9 +122,6 @@ bool findOperator(const PointSet& s0, const vector<Point>& data0, const vector<P
                                         << OUT(perm1) << OUT(perm2) << OUT(perm3) << OUT(sign1) << OUT(sign2)
                                         << OUT(sign3) << OUT(count); //    << OUT(move1) << OUT(move2) << OUT(move3);
                                     */
-                                    if (count >= 12) {
-                                        return true;
-                                    }
                                 }
                             }
                             /*
@@ -143,18 +148,16 @@ void first() {
     const auto data = input();
     cerr << data.size() << "x" << data[0].size() << endl;
 
-    PointSet result;
-    for (const auto& p : data[0]) {
-        result.insert(p);
-    }
+    PointSet result(ITRANGE(data[0]));
 
     vector<bool> good(data.size());
 
+    size_t its = 0;
     bool change = true;
-    vector<Point> data0 = data[0];
-    vector<Point> moves;
-    moves.emplace_back(Point::ZERO);
+    auto data0 = data[0];
+    Points moves = {Point::ZERO};
     while (change) {
+        ++its;
         change = false;
         for (int i = 1; i < data.size(); ++i) {
             if (good[i]) {
@@ -186,7 +189,7 @@ void first() {
         }
     }
 
-    cerr << good << endl;
+    LOG(INFO) << OUT(its) << OUT(good);
     cout << result.size() << endl;
     cout << "mx: " << max << endl;
 }
--- lib/header.h
+++ lib/header.h
@@ -386,4 +386,4 @@ string homeDir();
 
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define REP(i,n) FOR(i, 0, n)
-
+#define ITRANGE(v) (v).begin(), (v).end()
