1030e1afb60bcf0a92b6ef15d4ffb5c280217435
Den Raskovalov <denplusplus@yandex-team.ru>
2021-12-23 14:38:12 -0800

    reformat

--- lib/random.cpp
+++ lib/random.cpp
@@ -5,24 +5,20 @@ std::mt19937_64& getRandomNumberGenerator() {
     return gen;
 }
 
-template<>
+template <>
 int randAB(int a, int b) {
     std::uniform_int_distribution<int> d(a, b);
     return d(getRandomNumberGenerator());
 }
 
-int randInt() {
-    return randAB<int>(0, std::numeric_limits<int>::max());
-}
+int randInt() { return randAB<int>(0, std::numeric_limits<int>::max()); }
 
-char randChar() {
-    return 40 + (randInt() % 87);
-}
+char randChar() { return 40 + (randInt() % 87); }
 
 std::string randString(size_t len) {
     std::string result;
     result.resize(len);
-    for (char& ch: result) {
+    for (char& ch : result) {
         ch = randChar();
     }
     return result;
