b8dd346846b194af69fc29c5aeadce85327b2042
U-REDMOND\deraskov <denplusplus@yandex-team.ru>
2022-02-13 11:13:22 -0800

    more work about streams

--- lib/io/stream.cpp
+++ lib/io/stream.cpp
@@ -3,3 +3,42 @@
 InputStream::~InputStream() {}
 
 OutputStream::~OutputStream() {}
+
+    size_t StdOutputStream::write(const char* buffer, size_t toWrite) {
+        const auto res = write(1, buffer, toWrite);
+        if (res != toWrite) {
+            throw Exception("write to stdout failed");
+        }
+    }
+
+    void StdOutputStream::flush() {
+    }
+
+BufferedInputStream::BufferedInputStream(PInputStream nested) : nested_(nested) {}
+
+BufferedOutputStream::BufferedOutputStream(POutputStream nested, size_t bufferSize)
+    : nested_(nested), bufferSize_(bufferSize), buffer_(new char[bufferSize_]), bufferPos_(0) {}
+
+BufferedOutputStream::~BufferedOutputStream() {
+    flush();
+}
+
+
+size_t BufferedOutputStream::write(const char* buffer, size_t toWrite) {
+    while (toWrite) {
+        size_t canWrite = std::min(toWrite, bufferSize_ - bufferPos_);
+        memcpy(buffer_.get() + bufferPos_, buffer, canWrite);
+        toWrite -= canWrite;
+        buffer += canWrite;
+        if (bufferPos_ == bufferSize_) {
+            flush();
+        }
+    }
+}
+
+void BufferedOutputStream::flush() override {
+    if (bufferPos_) {
+        nested_->write(buffer_.get(), bufferPos_);
+        bufferPos_ = 0;
+    }
+}
--- lib/io/stream.h
+++ lib/io/stream.h
@@ -5,35 +5,55 @@
 using namespace std;
 
 class InputStream {
-public:
+   public:
     virtual ~InputStream();
 
     size_t read(char* buffer, size_t toRead) = 0;
 };
 
+using PInputStream = std::shared_ptr<InputStream>;
+
 class OutputStream {
-public:
+   public:
     virtual ~OutputStream();
 
     size_t write(const char* buffer, size_t toWrite) = 0;
+    void flush() = 0;
 };
 
+using POutputStream = std::shared_ptr<OutputStream>;
+
 class StdInputStream : public InputStream {
-public:
+   public:
     size_t read(char* buffer, size_t toRead) override;
 };
 
 class StdOutputStream : public OutputStream {
-public:
+   public:
     size_t write(const char* buffer, size_t toWrite) override;
+    void flush() override;
 };
 
 class BufferedInputStream : public InputStream {
-public:
+   public:
+    BufferedInputStream(PInputStream nested);
     size_t read(char* buffer, size_t toRead) override;
+
+   private:
+    PInputStream nested_;
 };
 
 class BufferedOutputStream : public OutputStream {
-public:
+   public:
+    static constexpr size_t DEFAULT_BUFFER_SIZE = 1 << 20;
+    BufferedOutputStream(POutputStream nested, size_t bufferSize = DEFAULT_BUFFER_SIZE);
+
     size_t write(const char* buffer, size_t toWrite) override;
+    void flush() override;
+
+   private:
+    POutputStream nested_;
+    size_t bufferSize_;
+    unique_ptr<char[]> buffer_;
+    size_t bufferPos_;
 };
