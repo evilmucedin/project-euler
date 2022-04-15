a3b145822cf8be057a19b7dc1d46d7bcb8c9cb10
root <denplusplus@yandex-team.ru>
2022-04-15 13:16:05 -0700

    move market data

--- ETFModeling/ETFModeling.cpp
+++ ETFModeling/ETFModeling.cpp
@@ -22,7 +22,7 @@ static const StringVector etfs = {"FBIOX", "FNCMX", "FSEAX", "FSKAX", "FSPSX", "
                                   "VOO",   "QQQ",   "BND",   "FBND",  "HDV",   "VEU",   "VWO",  "FDHY", "FDIS", "ONEQ",
                                   "VV",    "VB",    "HNDL",  "WBII",  "PCEF",  "FDIV",  "CEFS", "YLD",  "INKM", "IYLD",
                                   "FCEF",  "MLTI",  "YYY",   "MDIV",  "HIPS",  "CVY",   "GYLD", "VTI",  "VEA",  "IEFA",
-                                  "AGG",   "GLD",   "XLF",   "VNQ",   "LQD",   "SWPPX", "MGK"};
+                                  "AGG",   "GLD",   "XLF",   "VNQ",   "LQD",   "SWPPX", "MGK", "UNG", "OIH", "XME", "PFIX", "VXX", "EWZ", "ILF", };
 
 static const StringVector stocks = {
     "GOOG", "MSFT", "T", "NCLH", "OGZPY", "AMZN", "FB", "TSLA", "GME",
@@ -63,7 +63,7 @@ PriceData loadData(const StringVector& tickers) {
         Timer tFileRead("Read files");
         for (const auto& ticker : tickers) {
             DLOG(INFO) << "Ticker: " << ticker;
-            auto df = DataFrame::loadFromCsv(ticker + ".csv");
+            auto df = DataFrame::loadFromCsv("marketData/" + ticker + ".csv");
             auto date = df->getColumn("Date");
             auto price = df->getColumn("Adj Close");
             Date2Price tickerPrices;
--- ETFModeling/downloadData.py
+++ ETFModeling/downloadData.py
@@ -9,14 +9,14 @@ import yfinance
 # print(help(yfinance))
 
 tickers = ["FBIOX", "FNCMX", "FSEAX", "FSKAX", "FSPSX", "FXAIX", "GOOG", "IWM", "VUG", "MSFT", "T", "NCLH", "OGZPY",
-           "SPY", "IVV", "VOO", "QQQ", "AMZN", "FB", "TSLA", "BND", "FBND", "HDV", "VEU", "VWO", "FDHY", "FDIS", "ONEQ", "VV", "VB", "HNDL", "WBII", "PCEF", "FDIV", "CEFS", "YLD", "INKM", "IYLD", "FCEF", "MLTI", "YYY", "MDIV", "HIPS", "CVY", "GYLD", "VTI", "VEA", "IEFA", "AGG", "GLD", "XLF", "VNQ", "LQD", "SWPPX", "MGK", "GME"
+           "SPY", "IVV", "VOO", "QQQ", "AMZN", "FB", "TSLA", "BND", "FBND", "HDV", "VEU", "VWO", "FDHY", "FDIS", "ONEQ", "VV", "VB", "HNDL", "WBII", "PCEF", "FDIV", "CEFS", "YLD", "INKM", "IYLD", "FCEF", "MLTI", "YYY", "MDIV", "HIPS", "CVY", "GYLD", "VTI", "VEA", "IEFA", "AGG", "GLD", "XLF", "VNQ", "LQD", "SWPPX", "MGK", "GME", "UNG", "OIH", "XME", "PFIX", "VXX", "EWZ", "ILF"
            # "59101K877", "09259A395"
            ]
 assert(len(tickers) == len(set(tickers)))
 
 for ticker in tickers:
     data = yfinance.download(ticker, '2000-08-01', '2022-04-12')
-    data.to_csv(f"{ticker}.csv")
+    data.to_csv(f"marketData/{ticker}.csv")
 
 # Import the plotting library
 # %matplotlib inline
