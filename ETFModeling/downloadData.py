1f7cfd2c822633fb76ac70c46a6df07302b30e02
Den Raskovalov <denplusplus@yandex-team.ru>
2022-04-26 08:27:16 -0700

    refresh data

--- ETFModeling/downloadData.py
+++ ETFModeling/downloadData.py
@@ -19,7 +19,7 @@ tickers = ["FBIOX", "FNCMX", "FSEAX", "FSKAX", "FSPSX", "FXAIX", "GOOG", "IWM", 
 assert(len(tickers) == len(set(tickers)))
 
 for ticker in tickers:
-    data = yfinance.download(ticker, '2000-08-01', '2022-04-20', actions=True)
+    data = yfinance.download(ticker, '2000-08-01', '2022-04-25', actions=True)
     data.to_csv(f"marketData/{ticker}.csv")
 
 # Import the plotting library
