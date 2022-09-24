bb567f8e2d1ef651d5d80f2ecf7b0950afb1361a
Den Raskovalov <denplusplus@yandex-team.ru>
2022-09-24 13:46:52 -0700

    new project script

---
+++ leetcode/path-sum-ii/path-sum-ii.cpp
@@ -0,0 +1,34 @@
+#include "../header.h"
+
+class Solution {
+   public:
+    static void pathSum_(TreeNode* root, int targetSum, vector<vector<int>>& result, vector<int>& now) {
+        if (root == nullptr) {
+            return;
+        }
+        now.emplace_back(root->val);
+        targetSum -= root->val;
+        if (targetSum == 0 && !now.empty() && !root->left && !root->right) {
+            result.emplace_back(now);
+        }
+        pathSum_(root->left, targetSum, result, now);
+        pathSum_(root->right, targetSum, result, now);
+        now.pop_back();
+    }
+
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> result;
+        vector<int> now;
+        pathSum_(root, targetSum, result, now);
+        // result.erase(unique(result.begin(), result.end()), result.end());
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+    Timer t("Subarray timer");
+    cerr << sol.<< endl;
+
+    return 0;
+}
