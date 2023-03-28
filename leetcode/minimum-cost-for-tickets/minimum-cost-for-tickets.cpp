9ad73114bfe02d3d35506d40512750ec0f3fc376
Den Raskovalov <denplusplus@yandex-team.ru>
2023-03-28 10:25:52 -0700

    reformat

--- leetcode/minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ leetcode/minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,31 +1,33 @@
 #include "../header.h"
 
 class Solution {
-public:
+   public:
     static constexpr int DAYS[] = {1, 7, 30};
     static constexpr int INF = 1234567890;
 
-    int mincostTickets(const vector<int>& days_, const vector<int>& costs) {
-        auto days = days_;
-        // days.emplace_back(-1000);
-        sort(days.begin(), days.end());
+    int mincostTickets(const vector<int>& days, const vector<int>& costs) {
         const int n = days.size();
         vector<int> dp(n, INF);
         dp[0] = min({costs[0], costs[1], costs[2]});
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                if (i > 0) {
-                    dp[i] = min(dp[i], dp[i - 1] + costs[j]);
-                } else {
-                    dp[i] = min(dp[i], costs[j]);
+        for (int l = 0; l < 1; ++l) {
+            for (int i = 1; i < n; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    if (i > 0) {
+                        dp[i] = min(dp[i], dp[i - 1] + costs[j]);
+                    }
+                    int start = i - 1;
+                    while (start > 0 && days[start - 1] + DAYS[j] > days[i]) {
+                        --start;
+                    }
+                    cerr << OUT(i) << " " << OUT(j) << " " << OUT(start) << OUT(days[i]) << OUT(days[start]) << endl;
+                    if (start >= 0 && days[start] + DAYS[j] > days[i]) {
+                        dp[i] = min(dp[i], dp[start] + costs[j]);
+                    }
                 }
-                int start = i;
-                while (start >= 0 && days[start] + DAYS[j] > days[i]) {
-                    --start;
-                }
-                dp[i] = min(dp[i], dp[start] + costs[j]);
             }
         }
+
+        cerr << dp << endl;
 
         return dp.back();
     }
