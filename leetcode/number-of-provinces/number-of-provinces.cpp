531852d0526e2225d947b42ed1193ba9440ec5de
Den Raskovalov <denplusplus@yandex-team.ru>
2023-06-10 19:21:19 -0700

    AC

--- leetcode/number-of-provinces/number-of-provinces.cpp
+++ leetcode/number-of-provinces/number-of-provinces.cpp
@@ -1,9 +1,29 @@
 #include "../header.h"
 
+#include "lib/unionFindDense.h"
+
+class Solution {
+public:
+    int findCircleNum(const vector<vector<int>>& isConnected) {
+        const int n = isConnected.size();
+        UnionFindDense g(n);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (isConnected[i][j]) {
+                    g.unite(i, j);
+                }
+            }
+        }
+
+        return g.components().size();
+    }
+};
+
 int main() {
     Solution sol;
     Timer t("Subarray timer");
-    cerr << sol. << endl;
+    cerr << sol.findCircleNum({{1,1,0},{1,1,0},{0,0,1}}) << endl;
+    cerr << sol.findCircleNum({{1,0,0},{0,1,0},{0,0,1}}) << endl;
 
     return 0;
 }
