ed2f0f1ba2b0c1bdbee766172fa70dc15a9b3f9b
Den Raskovalov <denplusplus@yandex-team.ru>
2023-09-28 14:34:18 -0700

    fresh market data

--- leetcode/minimum-moves-to-spread-stones-over-grid/minimum-moves-to-spread-stones-over-grid.cpp
+++ leetcode/minimum-moves-to-spread-stones-over-grid/minimum-moves-to-spread-stones-over-grid.cpp
@@ -13,7 +13,9 @@ public:
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 3; ++j) {
                 if (grid[i][j] > 1) {
-                    rich.emplace_back(Point{i, j});
+                    for (int k = 0; k + 1 < grid[i][j]; ++k) {
+                        rich.emplace_back(Point{i, j});
+                    }
                 }
             }
         }
@@ -25,6 +27,15 @@ public:
                 }
             }
         }
+
+        vector<IntVector> m(rich.size(), IntVector(bich.size()));
+        for (int i = 0; i < rich.size(); ++i) {
+            for (int j = 0; j < bich.size(); ++j) {
+                m[i][j] = abs(rich[i].x - bich[j].x) + abs(rich[i].y - bich[j].y);
+            }
+        }
+
+        return hungarian(m).first;
     }
 };
 
@@ -32,6 +43,7 @@ int main() {
     Solution sol;
     Timer t("Subarray timer");
     cerr << sol.minimumMoves({{1, 1, 0}, {1, 1, 1}, {1, 2, 1}}) << endl;
+    cerr << sol.minimumMoves({{1, 3, 0}, {1, 0, 0}, {1, 0, 3}}) << endl;
 
     return 0;
 }
