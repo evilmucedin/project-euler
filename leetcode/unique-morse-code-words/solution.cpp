7dacc7220b11b3e10d685df673d1412241d91dcd
Den Raskovalov <denbox@gmail.com>
2025-08-26 20:37:23 -0700

    compile solution

--- leetcode/unique-morse-code-words/solution.cpp
+++ leetcode/unique-morse-code-words/solution.cpp
@@ -1,6 +1,23 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
+        static const vector<string> MORSE{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        cout << MORSE.size() << endl;
 
+        int result = 0;
+        return result;
     }
 };
+
+int main() {
+    Solution s;
+    vector<string> v1{"gin","zen","gig","msg"};
+    cout << s.uniqueMorseRepresentations(v1);
+    return 0;
+}
