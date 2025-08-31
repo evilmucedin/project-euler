20c8c448022d1fe2e69d6a989d3d27e853b63a3f
Den Raskovalov <denbox@gmail.com>
2025-08-31 15:41:43 -0700

    accepted

--- leetcode/positions-of-large-groups/solution.cpp
+++ leetcode/positions-of-large-groups/solution.cpp
@@ -7,10 +7,26 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> largeGroupPositions(string s) {
-        
+	vector<vector<int>> result;
+	const int n = s.size();
+	int a = 0;
+	for (int i = 1; i <= n; ++i) {
+		if (i == n || s[i] != s[a]) {
+            		if (i > a + 2) {
+				vector<int> v;
+				v.push_back(a);
+				v.push_back(i - 1);
+				result.push_back(v);
+			}
+			a = i;
+		}
+	}
+		
+	return result;	
     }
 };
 
 int main() {
+	Solution s;
 	return 0;
 }
