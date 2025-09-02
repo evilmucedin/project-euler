75267a5e8a73cc75b66dbd8dd081a54c287f9711
Den Raskovalov <denplusplus@yandex-team.ru>
2025-09-02 13:03:55 -0700

    start solutions

---
+++ leetcode/uncommon-words-from-two-sentences/solution.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+
+    }
+};
+
+int main() {
+    Solution s;
+    return 0;
+}
