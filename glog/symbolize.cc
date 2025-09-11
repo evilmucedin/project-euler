6d20615e782fc3693d34b868c766d25c612396cc
Den Raskovalov <denplusplus@yandex-team.ru>
2025-09-11 12:08:05 -0700

    fixes for BAZEL

--- glog/symbolize.cc
+++ glog/symbolize.cc
@@ -127,7 +127,7 @@ _END_GOOGLE_NAMESPACE_
 
 #include "symbolize.h"
 #include "config.h"
-#include "glog/raw_logging.h"
+#include "raw_logging.h"
 
 // Re-runs fn until it doesn't cause EINTR.
 #define NO_INTR(fn)   do {} while ((fn) < 0 && errno == EINTR)
