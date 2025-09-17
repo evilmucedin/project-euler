2ac43386c6785891b79ca3d03aea51b16d037164
Den Raskovalov <denplusplus@yandex-team.ru>
2025-09-16 21:43:40 -0700

    bazel

--- lib/init.cpp
+++ lib/init.cpp
@@ -1,10 +1,10 @@
-#include "glog/logging.h"
+#include "glog2/src/glog/logging.h"
 #include "gflags/gflags.h"
 
 #include "lib/header.h"
 
 void standardInit(int argc, char* argv[]) {
     ios::sync_with_stdio(false);
-    LoggingInitializer::get();
+    // LoggingInitializer::get();
     gflags::ParseCommandLineFlags(&argc, &argv, true);
 }
