92ecd97562d463508e49b37036d76aaef932ddfc
Den Raskovalov <denplusplus@yandex-team.ru>
2025-09-23 09:19:03 -0700

    implement init

--- lib/init.cpp
+++ lib/init.cpp
@@ -1,10 +1,26 @@
+#include <log4cplus/initializer.h>
+
 #include "glog2/src/glog/logging.h"
 #include "gflags/gflags.h"
 
 #include "lib/header.h"
 
+#include "lib/init.h"
+
+/*
+LoggingInitializer::LoggingInitializer() {
+  std::cout << "Logging system initialized." << std::endl;
+}
+
+LoggingInitializer& LoggingInitializer::get() {
+  static LoggingInitializer instance;
+  return instance;
+}
+
+*/
+
 void standardInit(int argc, char* argv[]) {
     ios::sync_with_stdio(false);
-    // LoggingInitializer::get();
+    LoggingInitializer::get();
     gflags::ParseCommandLineFlags(&argc, &argv, true);
 }
--- lib/init.h
+++ lib/init.h
@@ -1,3 +1,21 @@
 #pragma once
 
+
+class LoggingInitializer {
+ public:
+  static LoggingInitializer& get() {
+    static LoggingInitializer instance;
+    return instance;
+  }
+
+ private:
+  LoggingInitializer() {
+    std::cout << "Logging system initialized." << std::endl;
+  }
+  // Disallow copy and assign
+  LoggingInitializer(const LoggingInitializer&) = delete;
+  LoggingInitializer& operator=(const LoggingInitializer&) = delete;
+};
+
 void standardInit(int argc, char* argv[]);
+
