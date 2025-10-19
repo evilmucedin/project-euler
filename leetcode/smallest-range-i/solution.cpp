ed5df1376f24557cf19bce1e3ec6d74e35f7174d
Den Raskovalov <denplusplus@yandex-team.ru>
2025-10-19 14:34:30 -0700

    start LeetCode solution

---
+++ leetcode/smallest-range-i/solution.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int smallestRangeI(vector<int>& nums, int k) {
+
+    }
+};
+
+int main() {
+    Solution sol;
+    cout << sol.smallestRangeI();
+
+    return 0;
+}
