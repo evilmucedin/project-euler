1020d2d327f46192647e4c5ffbaaf8bac207616c
Den Raskovalov <denbox@gmail.com>
2025-10-20 16:39:51 -0700

    add solution

---
+++ leetcode/sort-array-by-parity/solution.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/sort-array-by-parity/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        vector<int> result;
+        return result;
+    }
+};
+
+int main() {
+    return 0;
+}
