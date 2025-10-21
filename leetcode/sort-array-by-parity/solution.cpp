c522bcf60ee13559622edbe69aad3eb24e67955e
Den Raskovalov <denbox@gmail.com>
2025-10-20 18:29:46 -0700

    solved

--- leetcode/sort-array-by-parity/solution.cpp
+++ leetcode/sort-array-by-parity/solution.cpp
@@ -8,6 +8,12 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         vector<int> result;
+        for (int i = 0; i < nums.size(); ++i)
+            if (nums[i] % 2 == 0)
+                result.push_back(nums[i]);
+        for (int i = 0; i < nums.size(); ++i)
+            if (nums[i] % 2 == 1)
+                result.push_back(nums[i]);
         return result;
     }
 };
