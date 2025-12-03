7847c4317cea07f33cabb7f0f80e1d9b7628b451
Den Raskovalov <denbox@gmail.com>
2025-12-02 18:58:14 -0800

    advent day 3

---
+++ advent/2025/3/3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#include "lib/header.h"
+#include "lib/string.h"
+
+#include "advent/lib/aoc.h"
+
+using namespace std;
+
+int main() {
+    const auto lines = readInputLines();
+
+
+    return 0;
+}
