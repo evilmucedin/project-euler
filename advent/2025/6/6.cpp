0ccc9705baa356741bef8f497e424562b2d91e29
Den Raskovalov <denbox@gmail.com>
2025-12-06 11:28:02 -0800

    day 6

--- advent/2025/6/6.cpp
+++ advent/2025/6/6.cpp
@@ -16,7 +16,44 @@ using namespace std;
 int main() {
     auto lines = readInputLines();
 
-    int sum = 0;
+    vector<vector<long long int>> v;
+    for (int i = 0; i + 1 < lines.size(); ++i) {
+        v.push_back({});
+        long long int x = 0;
+        for (char ch: lines[i]) {
+            if (ch >= '0' && ch <= '9')
+                x = 10*x + (ch - '0');
+            else {
+                if (x)
+                    v[i].push_back(x);
+                x = 0;
+            }
+        }
+        if (x)
+            v[i].push_back(x);
+    }
+
+    cerr << v << endl;
+
+    long long int sum = 0;
+    int vv = 0;
+    for (int i = 0; i < lines[lines.size() - 1].size(); ++i) {
+        if (lines[lines.size() - 1][i] == '+') {
+            long long int sum2 = 0;
+            for (int j = 0; j < v.size(); ++j)
+                sum2 += v[j][vv];
+            ++vv;
+            sum += sum2;
+        }
+        if (lines[lines.size() - 1][i] == '*') {
+            long long int sum2 = 1;
+            for (int j = 0; j < v.size(); ++j)
+                sum2 *= v[j][vv];
+            ++vv;
+            sum += sum2;
+        }
+        cerr << " " << i << " " << sum << endl;
+    }
 
     cout << sum << endl;
 
