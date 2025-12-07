27500f28c81d43bc04ae37c70a100ada7b7415cc
Den Raskovalov <denbox@gmail.com>
2025-12-06 17:02:17 -0800

    prepare day 7

---
+++ advent/2025/7/7.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#include <gmp.h>
+
+#include "lib/header.h"
+#include "lib/string.h"
+
+#include "advent/lib/aoc.h"
+
+using namespace std;
+
+int main() {
+    auto lines = readInputLines();
+
+    int sum = 0;
+
+    cout << sum << endl;
+
+    return 0;
+}
