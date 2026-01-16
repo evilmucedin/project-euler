b2904c1c9aa448b9ae9aa996e42425151e1634c6
Den Raskovalov <denbox@gmail.com>
2026-01-16 14:48:37 -0800

    start reading files

--- searchIndex/searchIndex.cpp
+++ searchIndex/searchIndex.cpp
@@ -1,3 +1,12 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 int main() {
+    for (int file = 1; file < 10; ++file) {
+        string filename = to_string(file) + ".txt";
+        cerr << filename << endl;
+    }
     return 0;
 }
