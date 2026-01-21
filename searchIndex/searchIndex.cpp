ee1a2c443fd52661d720d9fd2784407ad4782174
DenPlusPlus <Den.Raskovalov@shopify.com>
2026-01-21 11:40:12 -0800

    read data from files

--- searchIndex/searchIndex.cpp
+++ searchIndex/searchIndex.cpp
@@ -1,12 +1,32 @@
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 
 using namespace std;
 
+bool readFromFile(std::ifstream& file, string& s) {
+    s.clear();
+    while (s.size() == 0) {
+        if (!(file >> s))
+            return false;
+    }
+    return true;
+}
+
 int main() {
+    cerr << filesystem::current_path() << endl;
+
     for (int file = 1; file < 10; ++file) {
-        string filename = to_string(file) + ".txt";
+        const string filename = string("searchIndex/") + to_string(file) + ".txt";
         cerr << filename << endl;
+
+        std::ifstream istr(filename);
+        cerr << "!" << istr.is_open() << endl;
+        string s;
+        while (readFromFile(istr, s)) {
+            cerr << "  " << s << endl;
+        }
     }
     return 0;
 }
