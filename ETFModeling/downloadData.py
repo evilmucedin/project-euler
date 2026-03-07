cfc89e77766df286168a9050dab60219827a434b
Den Raskovalov <denbox@gmail.com>
2026-03-06 23:22:19 -0800

    refactor

--- ETFModeling/downloadData.py
+++ ETFModeling/downloadData.py
@@ -7,20 +7,7 @@ import matplotlib.pyplot as plt
 import yfinance
 from datetime import date, timedelta
 
-#   print(help(yfinance))
-
-tickers = ["FBIOX", "FNCMX", "FSEAX", "FSKAX", "FSPSX", "FXAIX", "SHOP", "GOOG", "IWM", "VUG", "MSFT", "T", "NCLH", "IBM",
-           "SPY", "IVV", "VOO", "QQQ", "AMZN", "META", "TSLA", "BND", "FBND", "HDV", "VEU", "VWO", "FDHY", "FDIS", "ONEQ", "VV", "VB", "HNDL", "WBII", "PCEF", "FDIV", "CEFS", "YLD", "INKM", "IYLD", "FCEF", "MLTI", "YYY", "MDIV", "HIPS", "CVY", "GYLD", "VTI", "VEA", "IEFA", "AGG", "GLD", "XLF", "VNQ", "LQD", "SWPPX", "MGK", "GME", "UNG", "OIH", "XME", "PFIX", "VXX", "EWZ", "ILF", "SCHE",
-           "AAPL", "NVDA", "TSM", "UNH", "JNJ", "V", "WMT", "JPM", "PG", "XOM", "HD", "CVX", "PFE", "VWAGY", "KHC", "NKE", "SBUX", "NBIS",
-           "FBCG", "FQAL", "FLPSX", "FDRR", "FMAG", "FPRO", "FBCV", "FMIL",
-           "COIN", "LMT", "FDIG", "ARKK", "FELG", "FTEC", "FDVV",
-           "BITO", "BITW", "VBB", "SFY", "IJR", "SCHD", "FTLS", "FDHT", "FRNW", "FDRV", "FCLD", "ITA", "PPA", "XAR", "USO", "IEO", "SPGP", "IWY", "SPYG", "DBO", "AIQ",
-           "BAMXF"
-           # "59101K877", "09259A395"
-           ]
-assert(len(tickers) == len(set(tickers)))
-
-for ticker in tickers:
+def downloadTicker(ticker):
     data = yfinance.download(ticker, start='2000-08-01', end=str(date.today() - timedelta(days=1)), actions=True, auto_adjust=False)
     filename = "marketData/%s.csv" % ticker
     data.to_csv(filename)
@@ -36,9 +23,25 @@ for ticker in tickers:
                 with open(filename + "", "w") as w:
                     w.writelines(lines)
 
+#   print(help(yfinance))
+
+tickers = ["FBIOX", "FNCMX", "FSEAX", "FSKAX", "FSPSX", "FXAIX", "SHOP", "GOOG", "IWM", "VUG", "MSFT", "T", "NCLH", "IBM",
+           "SPY", "IVV", "VOO", "QQQ", "AMZN", "META", "TSLA", "BND", "FBND", "HDV", "VEU", "VWO", "FDHY", "FDIS", "ONEQ", "VV", "VB", "HNDL", "WBII", "PCEF", "FDIV", "CEFS", "YLD", "INKM", "IYLD", "FCEF", "MLTI", "YYY", "MDIV", "HIPS", "CVY", "GYLD", "VTI", "VEA", "IEFA", "AGG", "GLD", "XLF", "VNQ", "LQD", "SWPPX", "MGK", "GME", "UNG", "OIH", "XME", "PFIX", "VXX", "EWZ", "ILF", "SCHE",
+           "AAPL", "NVDA", "TSM", "UNH", "JNJ", "V", "WMT", "JPM", "PG", "XOM", "HD", "CVX", "PFE", "VWAGY", "KHC", "NKE", "SBUX", "NBIS",
+           "FBCG", "FQAL", "FLPSX", "FDRR", "FMAG", "FPRO", "FBCV", "FMIL",
+           "COIN", "LMT", "FDIG", "ARKK", "FELG", "FTEC", "FDVV",
+           "BITO", "BITW", "VBB", "SFY", "IJR", "SCHD", "FTLS", "FDHT", "FRNW", "FDRV", "FCLD", "ITA", "PPA", "XAR", "USO", "IEO", "SPGP", "IWY", "SPYG", "DBO", "AIQ",
+           "BAMXF"
+           # "59101K877", "09259A395"
+           ]
+assert(len(tickers) == len(set(tickers)))
+
+for ticker in tickers:
+    downloadTicker(ticker)
+
 # Import the plotting library
 # %matplotlib inline
 
-# Plot the close price of the AAPL
-data['Adj Close'].plot()
-plt.show()
+# Plot the close price of the AAPL (uncomment and pass data if needed)
+# data['Adj Close'].plot()
+# plt.show()
