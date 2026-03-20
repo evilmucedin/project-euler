000072a5c9eb1bc0a29d7cbbe95a34bdfd4bce25
Den Raskovalov <denbox@gmail.com>
2026-03-20 10:34:11 -0700

    start Timus 2217

--- timus/2217/2217.cpp
+++ timus/2217/2217.cpp
@@ -11,41 +11,46 @@ int main() {
     string s;
 
     cin >> x >> s;
+    vector<int> a;
+    for (int i = 0; i < x; ++i)
+        if (s[i] == '0')
+            a.push_back(i+1); // 1-based positions
 
-    int a = 0;
-    int b = x - 1;
-    while (a < b) {
-        if (s[a] == s[b]) {
-            ++a;
-            --b;
-        } else {
-            if (s[a] < s[b]) {
-                break;
-            } else {
-                int c = b - 1;
-                while (a < c) {
-                    if (s[a] == s[c]) {
-                        std::swap(s[c], s[b]);
-                        // sort(s.begin() + a, s.begin() + c + 1);
-                        break;
-                    } else {
-                        --c;
-                    }
-                }
+    int k = (int)a.size();
+    int n = x;
+    int h = n/2;
 
-                if (s[a] != s[b])
-                    break;
-            }
+    bool possible = false;
+    for (int mid = 0; mid <= (n%2); ++mid) {
+        if ((k - mid) < 0) continue;
+        if ((k - mid) % 2 != 0) continue;
+        int m = (k - mid)/2;
+        if (m > h) continue;
+        if (mid == 1) {
+            if (m >= (int)a.size()) continue;
+            if (h+1 > a[m]) continue;
         }
+
+        // upper bounds ub[s] = A[s] for s=0..m-1
+        // lower bounds lb[s] = n - A[2*m+mid - s - 1] + 1
+        vector<int> ub(m), lb(m);
+        for (int sidx = 0; sidx < m; ++sidx) ub[sidx] = a[sidx];
+        for (int sidx = 1; sidx <= m; ++sidx) {
+            int idx = 2*m + mid - sidx; // 0-based
+            lb[sidx-1] = max(1, n - a[idx] + 1);
+        }
+
+        int cur = 1;
+        bool ok = true;
+        for (int sidx = 0; sidx < m; ++sidx) {
+            int need = max(lb[sidx], cur);
+            if (need > ub[sidx]) { ok = false; break; }
+            cur = need + 1;
+        }
+        if (ok) { possible = true; break; }
     }
-    // cerr << a << " " << b << endl;
 
-    bool can = (a >= b) || (x % 2 == 1 && a + 1 == b);
-
-    if (can)
-        cout << "YES" << endl;
-    else
-        cout << "NO" << endl;
+    cout << (possible ? "YES" : "NO") << endl;
 
     /*
     int x = 0;
