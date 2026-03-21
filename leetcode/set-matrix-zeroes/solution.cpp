ed912c1c7fd81d56c8246718a5ce63f486f9aa8b
Den Raskovalov <denbox@gmail.com>
2026-03-21 15:39:09 -0700

    start class Solution {

---
+++ leetcode/set-matrix-zeroes/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+        public void setZeroes(int[][] matrix) {
+
+                        }
+}
+
+int main() {
+    Solution s;
+    cout << s.setZeroes() << endl;
+    return 0;
+}
