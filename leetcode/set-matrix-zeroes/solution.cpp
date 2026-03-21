07c9adbfffbadb656a0a5b37ab67e0e736c05748
Den Raskovalov <denbox@gmail.com>
2026-03-21 16:50:08 -0700

    leetcode AC

--- leetcode/set-matrix-zeroes/solution.cpp
+++ leetcode/set-matrix-zeroes/solution.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,16 +7,17 @@ class Solution {
     public:
         void setZeroes(vector<vector<int>>& matrix) {
             const int n = matrix.size();
+            const int m = matrix[0].size();
             vector<int> x(n);
-            vector<int> y(n);
+            vector<int> y(m);
             for (int i = 0; i < n; ++i)
-                for (int j = 0; j < n; ++j)
+                for (int j = 0; j < m; ++j)
                     if (matrix[i][j] == 0) {
                         x[i] = 1;
                         y[j] = 1;
                     }
             for (int i = 0; i < n; ++i)
-                for (int j = 0; j < n; ++j)
+                for (int j = 0; j < m; ++j)
                     if (x[i] == 1 || y[j] == 1)
                         matrix[i][j] = 0;
         }
@@ -23,6 +25,7 @@ class Solution {
 
 int main() {
     Solution s;
+    vector<vector<int>> m = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
     // cout << s.setZeroes() << endl;
     return 0;
 }
