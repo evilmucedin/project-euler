ab3960431d57d69096cb6fb3f77ee00558db06bf
Den Raskovalov <denbox@gmail.com>
2026-04-15 22:11:34 -0700

    Ollama CLI

--- llmTest2/ollama.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import ollama
-
-response = ollama.generate(model='llama3', prompt='Write a poem about Linux.')
-
-with open('poem.txt', 'w') as f:
-    f.write(response['response'])
-
