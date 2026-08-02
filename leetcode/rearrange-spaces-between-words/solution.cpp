4381701697e6cdf38676af5ea1d0cd2d9fc4066c
Den Raskovalov <denbox@gmail.com>
2026-08-01 18:42:24 -0700

    fixes

---
+++ leetcode/rearrange-spaces-between-words/solution.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+    public:
+
+    String reorderSpaces(String text) {
+        const int n = text.len();
+
+    }
+};
+
+int main(void) {
+    Solution s;
+    cout << "`" << s.reorderSpaces(" practice   makes   perfect") << "'" << end;
+
+    return 0;
+}
